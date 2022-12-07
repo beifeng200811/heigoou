0e0da47faf7d702ceec97b4d1993c15b56b9773b
陈冰 <chenbing@aixuexi.com>
2022-12-07 19:32:37 +0800

    first commit

--- vendor/whistle/biz/webui/cgi-bin/rootca.js
+++
@@ -1,9 +0,0 @@
-var getRootCAFile = require('../../../lib/https/ca').getRootCAFile;
-
-module.exports = function(req, res) {
-  var type = req.query.type;
-  if (type !== 'cer' && type !== 'pem') {
-    type = 'crt';
-  }
-  res.download(getRootCAFile(), 'rootCA.' + type);
-};
